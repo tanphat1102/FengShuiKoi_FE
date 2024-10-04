2c8dc7cf0ee688ea3b3de12b9080de1d31fa4903
tanphat1102 <chopml1102@gmail.com>
2024-10-04 20:46:11 +0700

    login page

--- src/page/login/index.jsx
+++ src/page/login/index.jsx
@@ -1,90 +1,98 @@
-import React from 'react'
-import AuthenTemplate from '../../components/authen-template'
-import {Button, Form, Input} from 'antd'
-import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
-import { googleProvider } from '../../config/firebase';
-import {Link, useNavigate} from "react-router-dom";
-import api from '../../config/axios';
-import { toast } from 'react-toastify';
+import React from "react";
+import AuthenTemplate from "../../components/authen-template";
+import { Button, Form, Input } from "antd";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { googleProvider } from "../../config/firebase";
+import GoogleButton from "react-google-button";
+import { Link, useNavigate } from "react-router-dom";
+import api from "../../config/axios";
+import { toast } from "react-toastify";
+import "./index.css"; // Make sure this matches your file name
 
 function LoginPage() {
   const navigate = useNavigate();
 
-  const handleLoginGoogle =  () => {
+  const handleLoginGoogle = () => {
     const auth = getAuth();
     signInWithPopup(auth, googleProvider)
       .then((result) => {
-        // This gives you a Google Access Token. You can use it to access the Google API.
         const credential = GoogleAuthProvider.credentialFromResult(result);
         const token = credential.accessToken;
-        // The signed-in user info.
         const user = result.user;
 
         console.log(user);
-        // IdP data available using getAdditionalUserInfo(result)
-        // ...
-      }).catch((error) => {
-        // Handle Errors here.
+      })
+      .catch((error) => {
         const errorCode = error.code;
         const errorMessage = error.message;
-        // The email of the user's account used.
         const email = error.customData.email;
-        // The AuthCredential type that was used.
         const credential = GoogleAuthProvider.credentialFromError(error);
-        // ...
       });
-  }
+  };
 
-  const handleLogin = async  (values) => {
-    try{
-      const response = await api.post('login', values);
-      console.log(response)
+  const handleLogin = async (values) => {
+    try {
+      const response = await api.post("login", values);
+      const { role, token } = response.data;
 
-      const {role, token} =response.data;
-      
       localStorage.setItem("token", token);
 
-      if(role === 'ADMIN'){
+      if (role === "ADMIN") {
         navigate("/dashboard");
       }
-
-    }catch(err){
-      toast.error(err.response.data)
+    } catch (err) {
+      toast.error(err.response.data);
     }
-  }
+  };
 
   return (
     <AuthenTemplate>
-      <Form
-        labelCol={{
-          span: 24
-        }}
-        onFinish={handleLogin}
-        >
-        <Form.Item label="Phone or Email" name="phone"
-        rules={[{
-          required: true,
-          message: "Please input your username!"
-        }]}>
-          <Input/>
-        </Form.Item>
+      <div>
+        <h2>Welcome to Feng Shui Koi! </h2>
+        <p>Please sign-in to your account and start the adventure</p>
+        <Form labelCol={{ span: 24 }} onFinish={handleLogin}>
+          <Form.Item
+            label="Phone or Email"
+            name="phone"
+            rules={[
+              {
+                required: true,
+                message: "Please input your username!",
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
 
-        <Form.Item label="Password" name="password"
-        rules={[{
-          required: true,
-          message: "Please input your password!"
-        }]}>
-          <Input.Password/>
-        </Form.Item>
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: "Please input your password!",
+              },
+            ]}
+          >
+            <Input.Password />
+          </Form.Item>
 
-        <Link to="/register">Dont have account? Register new account</Link>
+          <Link to="/register" className="register-link">
+            Don't have an account? Register a new account
+          </Link>
 
-        <Button type='primary' htmlType='submit'>Login</Button>
-
-        <Button onClick={handleLoginGoogle}>Login google</Button>
-      </Form>
+          <Button type="primary" htmlType="submit">
+            Login
+          </Button>
+        </Form>
+        <div className="googleButton">
+          <button class="login-with-google-btn" onClick={handleLoginGoogle}>
+            Login with Google
+          </button>
+        </div>
+      </div>
     </AuthenTemplate>
-  )
+  );
 }
 
-export default LoginPage
+export default LoginPage;
