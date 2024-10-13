234ff586a5b1691e4395c417d7865fd31f2d0b97
tanphat1102 <chopml1102@gmail.com>
2024-10-13 11:01:07 +0700

    Create homePage

--- src/page/home/index.jsx
+++ src/page/home/index.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
+import BlogList from "../../components/blogList";
+import Layout from "../../components/Layout";
+import Tabbled from "../../components/Tabbed-template";
+import KoisAds from "../../components/kois-ads";
+import PlanAds from "../../components/plan-ads";
+import TestimonialCarousel from "../../components/testimonial-slider";
+import "./HomePage.scss";
 
 function HomePage() {
   return (
-    <div>HomePage</div>
-  )
+    <Layout>
+      <div className="homepage-main-container">
+        <div className="homepage-container">
+          <div className="left-home">
+            <Tabbled></Tabbled>
+            <TestimonialCarousel></TestimonialCarousel>
+          </div>
+          <div className="right-home">
+            <KoisAds></KoisAds>
+            <PlanAds></PlanAds>
+          </div>
+        </div>
+        <BlogList></BlogList>
+      </div>
+    </Layout>
+  );
 }
 
-export default HomePage
+export default HomePage;
