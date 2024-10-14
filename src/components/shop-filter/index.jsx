dfad50fc60757a16950660602260158a0913d8e0
nhon8005 <lehoannhon0207@gmail.com>
2024-10-14 22:48:43 +0700

    update shop-page

--- src/components/shop-filter/index.jsx
+++ src/components/shop-filter/index.jsx
@@ -1,33 +1,95 @@
 import React from 'react'
-import { Breadcrumb } from 'antd';
+import { Breadcrumb, Button } from 'antd';
 import './ShopFilter.scss';
 
 function ShopFilter() {
+    const products = [
+        {
+          id: 1,
+          name: "Cá Koi KujaKu Nhật Size Lớn",
+          price: "5 triệu",
+          imageUrl:
+            "https://cakoinhat.com/wp-content/uploads/2019/09/Hikarimoyo500_2.jpg",
+        },
+        {
+          id: 2,
+          name: "Cá Koi KujaKu Nhật Size Lớn",
+          price: "5 triệu",
+          imageUrl:
+            "https://cakoinhat.com/wp-content/uploads/2019/09/Hikarimoyo500_2.jpg",
+        },
+        {
+          id: 3,
+          name: "Cá Koi KujaKu Nhật Size Lớn",
+          price: "5 triệu",
+          imageUrl:
+            "https://cakoinhat.com/wp-content/uploads/2019/09/Hikarimoyo500_2.jpg",
+        },
+        {
+          id: 4,
+          name: "Cá Koi KujaKu Nhật Size Lớn",
+          price: "5 triệu",
+          imageUrl:
+            "https://cakoinhat.com/wp-content/uploads/2019/09/Hikarimoyo500_2.jpg",
+        },
+      ];
+
   return (
     <div>
         <div className="shop-nav-bar">
             <div className="shop-nav-tile">
-                <li>Kim</li>
-                <li>Mộc</li>
-                <li>Thủy</li>
-                <li>Hỏa</li>
-                <li>Thổ</li>
+                <div className="nav-item">
+                    <li>Kim</li>
+                    <img src="src\assets\kim.png" alt="Kim" style={{
+                        height:'50px',
+                        width: '70px'
+                    }} />
+                </div>
+
+                <div className="nav-item">
+                    <li>Mộc</li>
+                    <img src="src\assets\mộc.png" alt="Kim" style={{
+                        height:'50px',
+                        width: '70px'
+                    }} />
+                </div>
+
+                <div className="nav-item">
+                    <li>Thủy</li>
+                    <img src="src\assets\thủy.png" alt="Kim" style={{
+                        height:'50px',
+                        width: '70px'
+                    }} />
+                </div>
+
+                <div className="nav-item">
+                    <li>Hỏa</li>
+                    <img src="src\assets\hỏa.png" alt="Kim" style={{
+                        height:'50px',
+                        width: '70px'
+                    }} />
+                </div>
+
+                <div className="nav-item">
+                    <li>Thổ</li>
+                    <img src="src\assets\thổ.png" alt="Kim" style={{
+                        height:'50px',
+                        width: '70px'
+                    }} />
+                </div>
             </div>
         </div>
 
         <Breadcrumb
         items={[
         {
-            title: 'Home',
+            title: 'Trang chủ',
         },
         {
-            title: <a href="">Application Center</a>,
+            title: <a href="">Sản phẩm</a>,
         },
         {
-            title: <a href="">Application List</a>,
-        },
-        {
-            title: 'An Application',
+            title: <a href="">Kim</a>,
         },
         ]}
     />
--- src/page/shopPage/index.jsx
+++ src/page/shopPage/index.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import Layout from "../../components/Layout";
 import './ShopPage.scss'
 import ShopFilter from "../../components/shop-filter";
+import ShopItems from "../../components/shop-items";
 
 function ShopPage() {
   return (
     <Layout>
       <ShopFilter/>
+      <ShopItems/>
+      <center>Xem thêm</center>
     </Layout>
   );
 }
