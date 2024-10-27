888e020504524623b69aab2f038055dceabf57df
tanphat1102 <chopml1102@gmail.com>
2024-10-27 22:42:26 +0700

    css fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,25 @@
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
 import App from "./App.jsx";
+import "./index.css";
+import StudentManagement from "./StudentManagement.jsx";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
+import { store } from "./redux/store.js";
+import { persistor } from "./redux/store.js";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { ConfigProvider } from "antd";
 
 createRoot(document.getElementById("root")).render(
   <>
-    <App />
-    <ToastContainer />
+    <ConfigProvider>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+          <ToastContainer />
+        </PersistGate>
+      </Provider>
+    </ConfigProvider>
   </>
 );
