eac4f33e69198bd1a9a764af987562d1e2332858
nhon8005 <lehoannhon0207@gmail.com>
2024-10-28 21:22:53 +0700

    fix import error

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,6 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import StudentManagement from "./StudentManagement.jsx";
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
 import { store } from "./redux/store.js";
