bd558467dc3814ca73b0033cecae7a0b2421fbb3
tanphat1102 <chopml1102@gmail.com>
2024-10-30 09:56:01 +0700

    Revert " fix import error"
    This reverts commit eac4f33e69198bd1a9a764af987562d1e2332858.

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,11 @@
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+<<<<<<< HEAD
+=======
+import StudentManagement from "./StudentManagement.jsx";
+import "react-toastify/dist/ReactToastify.css";
+>>>>>>> parent of eac4f33 ( fix import error)
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import App from "./App.jsx";
