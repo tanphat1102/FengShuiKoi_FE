1b023cf4b602624b46bffc2223a8612339cc9f4c
Tan Phat Tran <chopml1102@gmail.com>
2025-04-12 22:59:15 +0700

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { ghPages } from "vite-plugin-gh-pages";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/FengShuiKoi_FE/",
-  plugins: [react()],
+  plugins: [react(), ghPages()],
 });
